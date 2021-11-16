7b4c2ebaeeebf032cd193eef811314fafecfb005
zsj <zousjun@126.com>
2021-11-16 20:11:29 +0800

    更新脚本

--- 关闭知乎和csdn的弹窗.user.js
+++ 关闭知乎和csdn的弹窗.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         关闭知乎和csdn的弹窗
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.8
 // @description  关闭知乎和csdn每次进入都会弹窗登录的弹窗
 // @author       zsj
 // @match        *://www.zhihu.com/*
@@ -9,16 +9,46 @@
 // @match        *://blog.csdn.net/*
 // @homepageURL  https://github.com/zsjun/ygreaseMonkey.git
 // @icon         https://www.google.com/s2/favicons?domain=zhihu.com
+// @supportURL   https://github.com/zsjun/ygreaseMonkey/blob/main/%E5%85%B3%E9%97%AD%E7%9F%A5%E4%B9%8E%E5%92%8Ccsdn%E7%9A%84%E5%BC%B9%E7%AA%97.user.js
+// @updateURL    https://github.com/zsjun/ygreaseMonkey/issues
+// @downloadURL  https://github.com/zsjun/ygreaseMonkey/blob/main/%E5%85%B3%E9%97%AD%E7%9F%A5%E4%B9%8E%E5%92%8Ccsdn%E7%9A%84%E5%BC%B9%E7%AA%97.user.js
 // @grant        none
 // @license MIT
 // ==/UserScript==
+(function () {
+  let zhihuFlag = false;
+  let csdnFlag = false;
 
-(function() {
-    'use strict';
+  const removeLoginModal = (mutationsList) => {
+    for (const mutation of mutationsList) {
+      for (const target of mutation.addedNodes) {
+        if (target.nodeType !== 1) return;
+        const zhihuButton =
+          target && target.querySelector(".Modal-closeButton");
+        const csdnButtonParent =
+          target && target.querySelector(".passport-login-box");
+        let csdnButton;
 
-    window.onload = function() {
-        document.querySelector(".Modal-closeButton").click()
-        document.querySelector("..passport-login-container").style.display="none"
+        if (csdnButtonParent) {
+          csdnButton = csdnButtonParent.querySelector("span");
+        }
+
+        if (zhihuButton && !zhihuFlag) {
+          zhihuFlag = true;
+          zhihuButton.click();
+        }
+
+        if (csdnButton && !csdnFlag) {
+          csdnFlag = true;
+          csdnButton.click();
+        }
+      }
     }
-    // Your code here...
+  };
+
+  const observer = new MutationObserver(removeLoginModal);
+  observer.observe(document, {
+    childList: true,
+    subtree: true,
+  });
 })();
